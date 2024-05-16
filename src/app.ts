9155b302c2ecf371ad8fbb4f015cb549fd14a9b8
serge_p <serge.prysiazhnyi@gmail.com>
2024-05-16 19:06:22 +0200

    mock data

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express, { Request, Response } from 'express';
+import users from './mocks/users.json';
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
+});
+
+app.get('/users', (req: Request, res: Response) => {
+  res.json(users);
 });
 
 app.listen(port, () => {
